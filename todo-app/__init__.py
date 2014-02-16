852e6057eef58beacf07a5ad40a976d2a4dbb412
vagrant <vagrant@precise64.(none)>
2014-02-16 21:11:49 +0000

    Sync almost done

--- todo-app/__init__.py
+++ todo-app/__init__.py
@@ -34,15 +34,24 @@ def unix2datetime(d):
     return datetime.datetime.utcfromtimestamp(d)
 
 
+def parse_object(object):
+    object['client_timestamp'] = unix2datetime(object['client_timestamp'])
+    return object
+
+
 # Models
-class ToDo(ndb.Model):
+class Item(ndb.Model):
     text = ndb.StringProperty()
     tags = ndb.StringProperty(repeated=True)
-    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+    is_deleted = ndb.BooleanProperty(default=False)
+
+    server_timestamp = ndb.DateTimeProperty(auto_now=True)
+    client_timestamp = ndb.DateTimeProperty()
 
     def to_dict(self):
-        result = super(ToDo, self).to_dict()
-        result['timestamp'] = datetime2unix(self.timestamp)
+        result = super(Item, self).to_dict()
+        result['timestamp'] = datetime2unix(self.client_timestamp)
+        result['key'] = self.key.urlsafe()
 
         return result
 
@@ -60,21 +69,55 @@ def main():
 def sync():
     logging.info('sync request')
 
-    elements2add = []
+    # 1. obtener los nuevos elementos del servidor (incluye elementos nuevos y
+    #    elementos a borrar).
+    timestamp = unix2datetime(request.json['server_timestamp'])
+    items_unsynced = Item.query(Item.server_timestamp > timestamp).order(Item.server_timestamp).fetch()
 
-    for e in request.json['new_items']:
-        text = e['value']['text']
-        timestamp = unix2datetime(e['value']['timestamp'])
+    if items_unsynced:
+        last_server_timestamp = items_unsynced[-1].server_timestamp
+    else:
+        last_server_timestamp = unix2datetime(0)
 
-        elements2add.append(ToDo(text=text, timestamp=timestamp))
+    # 2. insertar en el servidor los nuevos elementos del cliente. obtener sus
+    #    claves para enviarlas de vuelta.
+    items_client = [Item(**parse_object(item['object'])) for item in request.json['items_new']]
+    ndb.put_multi(items_client)
 
-    ndb.put_multi(elements2add)
+    if items_client:
+        last_client_timestamp = max(items_client, key=lambda i: i.server_timestamp).server_timestamp
+    else:
+        last_client_timestamp = timestamp
 
-    timestamps = [unix2datetime(e['value']['timestamp'])
-                 for e in request.json['deleted_items']]
+    # 3. actualizar en el servidor los elementos marcados para borrar del
+    #    cliente y actualizar su server_timestamp.
+    items_update = ndb.get_multi([ndb.Key(urlsafe=item['value']['key']) for item in request.json['items_delete']])
 
-    if timestamps:
-        elements2del = ToDo.query(ToDo.timestamp.IN(timestamps)).fetch(keys_only=True)
-        ndb.delete_multi(elements2del)
+    for item in items_update:
+        item.is_deleted = True
 
-    return jsonify({'response': 'OK'})
+    ndb.put_multi(items_update)
+
+    if items_update:
+        last_update_timestamp = max(items_update, key=lambda i: i.server_timestamp).server_timestamp
+    else:
+        last_update_timestamp = timestamp
+
+    # 4. enviar la respuesta al cliente.
+    server_timestamp = datetime2unix(max(last_server_timestamp,
+                                         last_client_timestamp,
+                                         last_update_timestamp))
+    items_server, items_delete = [], []
+
+    for item in items_unsynced:
+        if item.is_deleted:
+            items_delete.append(item.to_dict())
+        else:
+            items_server.append(item.to_dict())
+
+    return jsonify({
+        'itemsServer': items_server,
+        'itemsDelete': items_delete,
+        'itemsClient': [item.to_dict() for item in items_client],
+        'serverTimestamp': server_timestamp
+    })
